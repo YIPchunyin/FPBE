fcc1f4c381345722e3d759e95d130305ccd2a55a
YIPchunyin <yejunyan66@gmail.com>
2024-11-25 14:31:12 +0800

    test11111111

--- src/models/Post.js
+++ src/models/Post.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   title: { type: String, required: true },
   img_path: { type: String, required: false },
-  content: { type: String, required: true },
-  short_content: { type: String, required: true },
+  content: { type: String, required: false },
+  short_content: { type: String, required: false },
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
