f817759f8a2ac6ccc5d02216ca723c029a86a448
YIPchunyin <yejunyan66@gmail.com>
2024-11-28 23:48:12 +0800

    test55

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,7 +2,7 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_TOKEN;
 //中介件功能
-const validateToken =  (req, res, next) => {
+const validateToken = (req, res, next) => {
   const authHeader = req.headers['authorization']; // 获取 Authorization 头
   if (!authHeader) {
     return res.status(403).json({ message: "Token is required" });
@@ -30,18 +30,19 @@ const extractUserId = (req, res, next) => {
 
   const token = authHeader.split(' ')[1]; // 提取 Bearer token
   if (!token) {
-    req.userId = false; // 如果 token 不存在，设置 userId 为 false
+    req.userId = '6743120ec66b000001006812'; // 如果没有提供 token，设置 userId 为 false
+    // req.userId = false; // 如果 token 不存在，设置 userId 为 false
     return next(); // 继续处理请求
   }
-  req.userId = '6743120ec66b000001006812'; // 如果没有提供 token，设置 userId 为 false
   // 验证 token
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
-      req.userId = false; // 如果验证失败，设置 userId 为 false
+    req.userId = '6743120ec66b000001006812'; // 如果没有提供 token，设置 userId 为 false
+      // req.userId = false; // 如果验证失败，设置 userId 为 false
     } else {
       req.userId = decoded.userId; // 将 userId 附加到请求对象
     }
     next(); // 继续处理请求
   });
 };
-module.exports = { validateToken,extractUserId };
+module.exports = { validateToken, extractUserId };
